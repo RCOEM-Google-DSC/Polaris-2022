8cc02eda27555d4244ed5e037036992f16b9692f
jhamadhav <jhamadhav28@gmail.com>
2022-11-27 22:40:14 +0530

    content and ghost update

--- assets/js/index.js
+++ assets/js/index.js
@@ -2,7 +2,7 @@ document.addEventListener("scroll", () => {
     let dif = document.documentElement.scrollTop;
     if (dif > 1960) {
         let num = (dif - 1960)
-        console.log(num)
+        // console.log(num)
         document.getElementById("mario").style.transform = `translateX(${num}px)`
     }
 })
